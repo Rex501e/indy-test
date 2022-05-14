92910fdc4e8d46fa602a216bc850e254be987cb1
Rex501e <45625652+Rex501e@users.noreply.github.com>
2022-05-14 13:35:30 +0200

    pause repas

--- Service/ReductionService.php
+++ Service/ReductionService.php
@@ -9,23 +9,21 @@ class ReductionService
 {
     public function reductionAskAnswer(RedeemInfo $redeemInfo, Promocode $promocode)
     {
-        $isOK = true;
-
+        $errorMain = [];
         foreach ($promocode->getRestrictions() as $key => $restriction){
-            switch ($key){
+            $result = $this->testRestriction($key, $redeemInfo, $restriction);
 
-                case '@age':
-                    if ($redeemInfo->getArguments()['age'] == null) {
-                        break;
-                    } else {
-                        $isOK = $this->checkAgeRestrictions($redeemInfo->getArguments()['age'], $restriction);
-                    }
-                    break;
+            if($result != []){
+                $errorMain[$key] = $result;
             }
+        }
 
-            if ($isOK !== true) {
-               return $isOK;
-            }
+        if($errorMain != []) {
+          return [
+              'promocode_name' => $promocode->getName(),
+              'status' => 'denied',
+              'reasons' => $errorMain
+          ];
         }
 
         return [
@@ -37,16 +35,84 @@ class ReductionService
         ];
     }
 
-    private function checkAgeRestrictions(mixed $age, mixed $restriction)
+    private function checkNumberRestrictions(mixed $value, mixed $restriction): array
     {
+        $error = [];
         foreach ($restriction as $key => $condition) {
             switch ($key){
                 case 'gt':
-                    return $age > $condition;
+                    if(!($value > $condition)){
+                        $error['gt'] = 'IsNotGt';
+                    }
+                    break;
 
                 case 'lt':
-                    return $age < $condition;
+                    if(!($value < $condition)){
+                        $error['lt'] = 'IsNotLt';
+                    }
+                    break;
+
+                case 'eq':
+                    if(!($value == $condition)){
+                        $error['eq'] = 'IsNotEq';
+                    }
+                    break;
             }
         }
+
+        return $error;
+    }
+
+    private function testRestriction(int|string $key, RedeemInfo $redeemInfo, mixed $restriction): array
+    {
+        $error = [];
+        switch ($key) {
+            case '@age':
+                if ($redeemInfo->getArguments()['age'] == null) {
+                    $error[$key] = 'Age value is missing';
+                } else {
+                    $checkNumberRestrictions = $this->checkNumberRestrictions($redeemInfo->getArguments()['age'], $restriction);
+                    if ($checkNumberRestrictions != []) {
+                        $error[$key] = $checkNumberRestrictions;
+                    }
+                }
+                break;
+
+            case '@date':
+                if ($redeemInfo->getArguments()['date'] == null) {
+                    $error[$key] = 'Age value is missing';
+                } else {
+                    $checkDateRestrictions = $this->checkDateRestrictions($redeemInfo->getArguments()['date'], $restriction);
+                    if ($checkDateRestrictions != []) {
+                        $error[$key] = $checkDateRestrictions;
+                    }
+                }
+        }
+
+        // si $error n'est pas vide, c'est qu'il y a eu une erreur dans l'un des cas
+        // false et true et true => false
+        return $error;
+    }
+
+    private function checkDateRestrictions(mixed $date, mixed $restriction): array
+    {
+        $error = [];
+        foreach ($restriction as $key => $condition) {
+            switch ($key){
+                case 'before':
+                    if(!(strtotime($date) < strtotime($condition))){
+                        $error['before'] = 'IsNotBefore';
+                    }
+                    break;
+
+                case 'after':
+                    if(!(strtotime($date) > strtotime($condition))){
+                        $error['after'] = 'IsNotAfter';
+                    }
+                    break;
+            }
+        }
+
+        return $error;
     }
 }
--- Test/Test.php
+++ Test/Test.php
@@ -42,4 +42,95 @@ class Test extends TestCase
             $reductionService->reductionAskAnswer($redeemInfo, $promoCode)
         );
     }
+
+    public function testSimple2ReductionAskAnswer()
+    {
+        $reductionService = new ReductionService();
+
+        $promoCode = new Promocode();
+        $promoCode->setName("WeatherCodeAgeSimple");
+        $promoCode->setAvantage(['percent' =>  25]);
+        $promoCode->setRestrictions([
+            '@age' => [
+                'gt' => 10,
+                'lt' => 20,
+            ],
+            '@date' => [
+                'after' => '2021-01-01',
+                'before' => '2022-01-01'
+            ],
+        ]);
+
+        $redeemInfo = new RedeemInfo();
+        $redeemInfo->setPromocodeName('WeatherCodeAge');
+        $redeemInfo->setArguments([
+            'age' => 9,
+            'date' => '2020-03-02'
+        ]);
+
+        $this->assertSame(
+            [
+                'avantage' => [
+                    'percent' => 25
+                ],
+                'promocode_name' => 'WeatherCodeAgeSimple',
+                'status' => 'accepted'
+            ],
+            $reductionService->reductionAskAnswer($redeemInfo, $promoCode)
+        );
+    }
+
+    public function testComplexReductionAskAnswer()
+    {
+        $reductionService = new ReductionService();
+
+        $promoCode = new Promocode();
+        $promoCode->setName("WeatherCodeAgeComplex");
+        $promoCode->setAvantage(['percent' =>  20]);
+        $promoCode->setRestrictions([
+            '@or' => [
+                0 => [
+                    '@age' => [
+                        'eq' => 40,
+                    ],
+                ],
+                1 => [
+                    '@age' => [
+                        'gt' => 15,
+                        'lt' => 30,
+                    ],
+                ],
+            ],
+            '@date' => [
+                'after' => '2021-01-01',
+                'before' => '2022-01-01'
+            ],
+            '@meteo' => [
+                'is' => 'clear',
+                'temp' => [
+                    'lt' => 100
+                ],
+            ],
+        ]);
+
+        $redeemInfo = new RedeemInfo();
+        $redeemInfo->setPromocodeName('WeatherCodeAgeComplex');
+        $redeemInfo->setArguments([
+            'age' => 16,
+            'meteo' => [
+                'town' => 'Lyon'
+            ],
+        ]);
+
+        $this->assertSame(
+            [
+                'avantage' => [
+                    'percent' => 20
+                ],
+                'promocode_name' => 'WeatherCodeAgeComplex',
+                'status' => 'accepted'
+            ],
+            $reductionService->reductionAskAnswer($redeemInfo, $promoCode)
+        );
+    }
 }
